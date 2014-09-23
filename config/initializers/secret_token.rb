ae7fbd29521e44350cc76d74b210ddcae50e65ce
programoo <proprakarn@gmail.com>
2014-09-23 11:49:24 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PortoApp::Application.config.secret_key_base = '7d3d6c59599a8a5e185593ce7ff23720cbb25e55d34965b770e9becd7ec603c4af023c56cd3c22a627e181348336fa5ba517bf0c669cf16100d4d994e3b14584'
